3bbe141dcf2f1b96b68950343b552b5272f965af
Frank Heider <heider669@googlemail.com>
2020-06-10 09:36:02 +0200

    fix include path, add default to sass variables

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,21 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
 <head>
-    @include('_layout.html.head')
+    @include('_ui.html.head')
 </head>
 <body class="h-100 d-flex flex-column">
 
     <header>
-        @include('_layout.html.navbar-top')
-        @include('_layout.html.navbar-second')
+        @include('_ui.html.navbar-top')
+        @include('_ui.html.navbar-second')
     </header>
 
     <main class="container">
         @yield('content')
-        @include('_layout.html.footer')
+        @include('_ui.html.footer')
     </main>
 
-    @include('_layout.html.scripts')
+    @include('_ui.html.scripts')
 
 </body>
 </html>
